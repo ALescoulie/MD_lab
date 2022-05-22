9ef31034ed23c1a5aa2e4a5a491d9fc3ab1d5b58
Alia Lescoulie <alia.lescoulie@gmail.com>
2022-05-22 08:15:49 -0700

    implement getters and setters in box

--- src/CubicBox.cpp
+++ src/CubicBox.cpp
@@ -17,7 +17,12 @@ void CubicBox::add_atom(Atom atom) {
 }
 
 void CubicBox::update_pos(Vec3* new_pos) {
+    if (prev_pos != nullptr) {
+        free(prev_pos);
+    }
 
+    prev_pos = positions;
+    positions = new_pos;
 }
 
 int CubicBox::get_n_atoms() {
@@ -29,5 +34,27 @@ Atom CubicBox::get_atom(int ind) {
 }
 
 void CubicBox::update_vel(Vec3* new_vel) {
-
+    if (velocities != nullptr) {
+        free(velocities);
+    }
+    velocities = new_vel;
 }
+
+Vec3 CubicBox::get_pos(int ind) {
+    return Vec3(positions[ind].x, positions[ind].y, positions[ind].z);
+}
+
+Vec3 CubicBox::get_prev_pos(int ind) {
+    return Vec3(prev_pos[ind].x, prev_pos[ind].y, prev_pos[ind].z);
+}
+
+Vec3 CubicBox::get_vel(int ind) {
+    return Vec3(velocities[ind].x, velocities[ind].y, velocities[ind].z);
+}
+
+Vec3 CubicBox::get_mom(int ind) {
+    auto mom = Vec3(velocities[ind].x, velocities[ind].y, velocities[ind].z);
+    mom.vec_scale(atoms[ind].get_mass());
+    return mom;
+}
+
