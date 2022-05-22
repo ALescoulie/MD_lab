eecb51a4f00879a994e2e985521b05bf074a123d
Alia Lescoulie <alia.lescoulie@gmail.com>
2022-05-22 09:52:57 -0700

    fix input generator

--- utils/input_gen.py
+++ utils/input_gen.py
@@ -8,18 +8,18 @@ Arguments
 import sys
 
 yaml = [
-    "topology:",
-    "output:",
-    "# Do not change, these are the only options in this version"
-    "force_field: lj",
-    "boundary: periodic",
-    "thermostat: random",
-    "# Time units in fs, distance units in nm, temp units in K",
-    "box_size:"
-    "temp"
-    "timestep:"
-    "time:"
-    "record_freq:"
+    'topology:',
+    'output:',
+    '# Do not change, these are the only options in this version',
+    'force_field: "lj"',
+    'boundary: "periodic"',
+    'thermostat: "random"',
+    '# Time units in fs, distance units in nm, temp units in K',
+    'box_size:',
+    'temp:',
+    'timestep:',
+    'time:',
+    'record_freq:'
 ]
 
 if __name__ == '__main__':
